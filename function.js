c128efc58161b677476fdb4e1f2cb0bfa6b4ae56
thr3000 <fengb702@gmail.com>
2020-09-01 13:55:12 -0700

    updated contact

---
+++ function.js
@@ -0,0 +1,17 @@
+function jump(val) {
+  if (val == 1) {
+    window.location.href = "about.html";
+  }
+  else if (val == 2) {
+    window.location.href = "project.html";
+  }
+  else if (val == 3) {
+    window.location.href = "contact.html";
+  }
+  else if (val == 4){
+    window.location.href = "course.html";
+  }
+  else {
+    window.location.href = "index.html";
+  }
+}
